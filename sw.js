65526ab92ab7a9f2cd4778267b5a62ac0d94a5f3
Affan-Shaikh2004 <sanjanakamble518@gmail.com>
2026-04-28 10:38:37 +0530

    Fix paths for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
 const CACHE_NAME = "shop-pwa-v1";
-const ASSETS = ["/", "/index.html", "/style.css", "/app.js"];
+const ASSETS = [
+  "/ecommerce-pwa/",
+  "/ecommerce-pwa/index.html",
+  "/ecommerce-pwa/style.css",
+  "/ecommerce-pwa/app.js"
+];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
